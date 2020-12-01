0e4d240b862ba317de4ebe60d0f052f2d03449b0
Steven Nguyen <stevennguyen901@hotmail.com>
2020-11-30 23:27:21 -0800

    Add JS file

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+const notesArray = [
+    { title: "note one", body: "some text 1"},
+    { title: "note two", body: "some text 2"},
+]
+
+const drkModeBtn = document.querySelector('.themebtn')
+
+const noteBtn = document.querySelector('.notebtn')
+
+console.log(notesArray)
